2761c80814a16901c33692ce5771e27eb0401712
Bryan Tobin <bryanfctobin@gmail.com>
2018-12-03 22:13:15 -0500

    Updated the sentence for each birthday response

--- talkpython_days/01-03-datetimes/bryan/game.py
+++ talkpython_days/01-03-datetimes/bryan/game.py
@@ -15,6 +15,6 @@ secondagedelta = (today.year - age - 1)
 birthdayoccur = raw_input("Has your birthday already passed this year?  Please respond with 'y' for yes, and 'n' for no.")
 
 if birthdayoccur == 'y':
-    print("You were born in " + str(agedelta))
+    print("You were born in " + str(agedelta) + ", man you are old!")
 else:
-    print("You were born in " + str(secondagedelta))
+    print("You were born in " + str(secondagedelta) + ", man you are old!")
